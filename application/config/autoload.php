e69a4583d238c15970bfd712cc6cba744e05fb8d
hbc <bcxxxxxx@gmail.com>
2013-03-28 16:46:25 +0800

    enabled session & database

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -79,7 +79,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'email', 'xmlrpc');
 */
 
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('database');
 
 
 /*
@@ -95,7 +95,7 @@ $autoload['libraries'] = array();
 |	$autoload['drivers'] = array('session', 'cache');
 */
 
-$autoload['drivers'] = array();
+$autoload['drivers'] = array('session');
 
 
 /*
