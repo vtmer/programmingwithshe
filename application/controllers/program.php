c639b2f209b48bf0d1d6770d71749e519801419b
Maureen <Maureencqm@gmail.com>
2013-04-04 11:24:00 +0800

    connect data to solution table

--- application/controllers/program.php
+++ application/controllers/program.php
@@ -3,10 +3,11 @@ class Program extends CI_Controller {
     public function __construct()
     {
         parent:: __construct();
-        $this->load->model('test_model');
+        $this->load->model('solution_model');
         $this->load->helper('form');
         $this->load->library('form_validation');
     }
+
     public function index()
     {
     }
@@ -14,19 +15,23 @@ class Program extends CI_Controller {
 
     public function setContent()
     {
+        $content = $this->input->post('content');
         $this->form_validation->set_rules('content','content','required');
-        if($this->form_validation->run()===FALSE)
+        if ($this->form_validation->run() === FALSE)
         {
             $this->load->view('setContent');
             $this->load->view('footer');
         }
         else
         {
-            $this->test_model->set_content();
+            $this->solution_model->set_content($content);
+            $source_id = $this->solution_model->get_max_id();
+            $this->solution_model->set_solution($source_id);
             $this->load->view('header');
             $this->load->view('success');
         }
             
     }
+
 }
 ?>
