18d02e9d0ff2c327f6a894bc0b156574465c3c25
hbc <bcxxxxxx@gmail.com>
2013-04-19 12:28:35 +0800

    removed unused file

--- application/views/add_tutor.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo validation_errors();
-echo form_open('backend/tutor/create');
-?>
-<form>
-<label for="create_tutor">Tutor:</label>
-<textarea id="create_tutor" name="create_tutor" rows="10" cols="30"></textarea>
-<input type="submit" name="submit" value="submit"/>
-</form>
--- application/views/footer.php
+++
@@ -1,3 +0,0 @@
-<div>VTM STUDIO &copy 2013</div>
-</body>
-</html>
